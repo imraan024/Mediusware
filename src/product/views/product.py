45d90945ccda8ac2c8f6eeaa5c001f3fa6be4dc0
imraan024 <asq24i@gmail.com>
2021-12-24 02:43:53 +0600

    filtering works partialy

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -2,7 +2,9 @@ from django.views import generic, View
 from product.models import ProductVariant, ProductVariantPrice, Variant, Product
 from django.views.generic import ListView
 from django.core.paginator import Paginator
-
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.db.models import Q
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
@@ -18,6 +20,29 @@ class ProductListView(generic.TemplateView):
     template_name = 'products/list.html'
     model = Product
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = Product.objects.all()
+        product_var = Variant.objects.all()
+        product_var_price = ProductVariantPrice.objects.all()
+        context['product'] = product
+        context['variants'] = product_var
+        context['product_variant_price'] = product_var_price
+        return context
+
+class ProductSearchView(ListView):
+    template_name = 'products/filter.html'
+    model = Product
+    #success_url = reverse_lazy('list.product')
+
+    def get_queryset(self):
+        title = self.request.GET.get('title')
+        variant = self.request.GET.get('variant')
+        object_list = Product.objects.filter(
+            Q(title__icontains=title) 
+        )
+        return object_list
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         product = Product.objects.all()
